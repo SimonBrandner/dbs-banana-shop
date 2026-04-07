153db52cc422c5d0366abc3d4663f4fd10bd6f98
Šimon Brandner <simon.bra.ag@gmail.com>
2026-04-07 17:09:30 +0200

    Use restrict instead of cascade
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- docs/checkpoints/cp3/schema/main.sql
+++ docs/checkpoints/cp3/schema/main.sql
@@ -12,19 +12,19 @@ CREATE TABLE person (
 
 CREATE TABLE phone_number (
     phone_number varchar(255) NOT NULL,
-    person_id uuid NOT NULL REFERENCES person(person_id) ON DELETE CASCADE,
+    person_id uuid NOT NULL REFERENCES person(person_id) ON DELETE RESTRICT,
     UNIQUE (phone_number),
     PRIMARY KEY (phone_number)
 );
 
 CREATE TABLE employee (
-    person_id uuid NOT NULL REFERENCES person(person_id) ON DELETE CASCADE,
+    person_id uuid NOT NULL REFERENCES person(person_id) ON DELETE RESTRICT,
     UNIQUE (person_id),
     PRIMARY KEY (person_id)
 );
 
 CREATE TABLE customer (
-    person_id uuid NOT NULL REFERENCES person(person_id) ON DELETE CASCADE,
+    person_id uuid NOT NULL REFERENCES person(person_id) ON DELETE RESTRICT,
     UNIQUE (person_id),
     PRIMARY KEY (person_id)
 );
@@ -44,36 +44,36 @@ CREATE TABLE banana (
     shelf_column_number integer NOT NULL,
     UNIQUE (barcode),
     PRIMARY KEY (barcode),
-    FOREIGN KEY (shelf_row_number, shelf_column_number) REFERENCES shelf(row_number, column_number) ON DELETE CASCADE
+    FOREIGN KEY (shelf_row_number, shelf_column_number) REFERENCES shelf(row_number, column_number) ON DELETE RESTRICT
 );
 
 CREATE TABLE banana_order(
     creation_time TIMESTAMPTZ NOT NULL,
-    customer_person_id uuid REFERENCES customer(person_id) ON DELETE CASCADE,
-    employee_person_id uuid REFERENCES employee(person_id) ON DELETE CASCADE,
+    customer_person_id uuid REFERENCES customer(person_id) ON DELETE RESTRICT,
+    employee_person_id uuid REFERENCES employee(person_id) ON DELETE RESTRICT,
     UNIQUE (creation_time, customer_person_id),
     PRIMARY KEY (creation_time, customer_person_id)
 );
 
 CREATE TABLE supervises(
-    junior uuid NOT NULL REFERENCES employee(person_id) ON DELETE CASCADE,
-    senior uuid NOT NULL REFERENCES employee(person_id) ON DELETE CASCADE,
+    junior uuid NOT NULL REFERENCES employee(person_id) ON DELETE RESTRICT,
+    senior uuid NOT NULL REFERENCES employee(person_id) ON DELETE RESTRICT,
     UNIQUE (junior),
     PRIMARY KEY (junior)
 );
 
 CREATE TABLE can_get_for_free(
-    customer_person_id uuid NOT NULL REFERENCES customer(person_id) ON DELETE CASCADE,
-    banana_barcode uuid NOT NULL REFERENCES banana(barcode) ON DELETE CASCADE,
+    customer_person_id uuid NOT NULL REFERENCES customer(person_id) ON DELETE RESTRICT,
+    banana_barcode uuid NOT NULL REFERENCES banana(barcode) ON DELETE RESTRICT,
     UNIQUE (customer_person_id, banana_barcode),
     PRIMARY KEY (customer_person_id, banana_barcode)
 );
 
 CREATE TABLE order_contains_banana(
-    banana_barcode uuid NOT NULL REFERENCES banana(barcode) ON DELETE CASCADE,
+    banana_barcode uuid NOT NULL REFERENCES banana(barcode) ON DELETE RESTRICT,
     customer_person_id uuid NOT NULL,
     order_creation_time TIMESTAMPTZ NOT NULL,
     UNIQUE (banana_barcode),
     PRIMARY KEY (banana_barcode),
-    FOREIGN KEY (order_creation_time, customer_person_id) REFERENCES banana_order(creation_time, customer_person_id) ON DELETE CASCADE
+    FOREIGN KEY (order_creation_time, customer_person_id) REFERENCES banana_order(creation_time, customer_person_id) ON DELETE RESTRICT
 );
