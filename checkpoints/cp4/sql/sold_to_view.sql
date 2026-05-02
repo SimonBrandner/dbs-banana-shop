6d9e4046a7be094f756c031d4b8a2ce14b27eb05
Šimon Brandner <simon.bra.ag@gmail.com>
2026-05-02 12:09:34 +0200

    Simplify view
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- checkpoints/cp4/sql/sold_to_view.sql
+++ checkpoints/cp4/sql/sold_to_view.sql
@@ -1,10 +1,9 @@
-CREATE VIEW sold_to_view AS
+CREATE
+OR REPLACE VIEW sold_to_view AS
 SELECT
     employee_person.name AS employee_name,
     customer_person.name AS customer_name
 FROM
     banana_order
-    INNER JOIN customer ON banana_order.customer_person_id = customer.person_id
-    INNER JOIN person customer_person ON customer.person_id = customer_person.person_id
-    INNER JOIN employee ON banana_order.employee_person_id = employee.person_id
-    INNER JOIN person employee_person ON employee.person_id = employee_person.person_id;
+    INNER JOIN person customer_person ON banana_order.customer_person_id = customer_person.person_id
+    INNER JOIN person employee_person ON banana_order.employee_person_id = employee_person.person_id;
