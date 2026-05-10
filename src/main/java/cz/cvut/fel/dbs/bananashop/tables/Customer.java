b0f46f6dcdc521100492792aaff780512cdc8a13
Ondřej Čopák <ondra.copak@gmail.com>
2026-05-10 17:50:18 +0200

    deleted wrongly tracked files

--- src/main/java/cz/cvut/fel/dbs/bananashop/tables/Customer.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fel.dbs.bananashop.tables;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "customer")
-public class Customer extends Person {
-    public Customer() {}
-}
